2d3dc9d6e5cce0af6a6c5011eb62920a6beaa733
Jorge Neto <jorge.c.d.s.neto@gmail.com>
2023-01-12 13:21:32 -0300

    começo do script

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,19 @@
+let cards = prompt("Escolhe um número de cartas entre 4 e 14");
+
+if (cards % 2 !== 0) {
+    
+    while (cards % 2 !==0) {
+        cards = prompt("Escolhe um número de cartas entre 4 e 14");
+    }
+    
+} else if(cards < 4 || cards > 14){
+    
+    while (cards < 4 || cards > 14) {
+        cards = prompt("Escolhe um número de cartas entre 4 e 14");
+    }
+    
+}
 //if para achar o numero
 
 //usar o numero digitado para botar as cartas com um while
+
